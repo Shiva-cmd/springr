e7655b5d0e595781f054952628bef2e0ce3a9f28
Shivakumar-cmd <sshhiivvaa1996@gmail.com>
2021-05-18 23:15:47 +0530

    second

--- src/homedetailScreen.js
+++ src/homedetailScreen.js
@@ -1,10 +1,51 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import {Text, View} from 'react-native';
-export function HomeScreendetail() {
+import {Button, Text, View, Image} from 'react-native';
+export function HomeScreendetail(props) {
+  console.log(props)
   return (
-    <View>
-      <Text>HOMME DETAIL</Text>
+    <View style={{flex: 1, margin: 10}}>
+      <View
+        style={{
+          justifyContent: 'center',
+          alignItems: 'center',
+          marginBottom: 10,
+        }}>
+        <Text style={{color: 'black', fontSize: 18, fontWeight: '700'}}>
+          News
+        </Text>
+      </View>
+      <View>
+        <Image
+          source={{
+            uri: 'https://reactnative.dev/img/tiny_logo.png',
+          }}
+          style={{
+            width: '100%',
+            height: 150,
+            resizeMode: 'stretch',
+          }}></Image>
+      </View>
+      <View>
+        <Button title="Share"></Button>
+      </View>
+      <View>
+        <Text>HOMME DETAIL</Text>
+      </View>
+      <View>
+        <View>
+          <Text>name</Text>
+        </View>
+        <View>
+          <Text>share</Text>
+        </View>
+        <View>
+          <Text>Daete</Text>
+        </View>
+      </View>
+      <View>
+        <Text>Descriptio</Text>
+      </View>
     </View>
   );
 }
--- src/homescreen.js
+++ src/homescreen.js
@@ -1,14 +1,103 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable react-native/no-inline-styles */
 /* eslint-disable prettier/prettier */
-import React from 'react';
-import {Button, Text, View} from 'react-native';
-export function HomeScreen(props) {
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+import {
+  Button,
+  ScrollView,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  ActivityIndicator,
+} from 'react-native';
+
+const HomeScreen = props => {
+  const [data, setData] = useState([]);
+  const [loading, setloading] = useState(true);
+  useEffect(() => {
+    Api();
+  }, []);
+  async function Api() {
+    try {
+      const response = await axios.get(
+        'https://newsapi.org/v2/sources?apiKey=d29d58aab88d4ea0b04ddb245a230068',
+      );
+      // console.log(response.data.sources);
+     
+      
+      const propertyNames = Object.values(response.data.sources);
+       setData(propertyNames);
+      //  console.log(data)
+      setloading(false);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  
   return (
-    <View style={{flex:1,justifyContent:'center',backgroundColor:'red'}}>
-      <Text>HOMME </Text>
-      <Button title="prsee me" onPress={()=>props.navigation.navigate('Homedeatl')}/>
+    <View style={{margin: 10, flex: 1}}>
+      {loading ? (
+        <View
+          style={{
+            justifyContent: 'center',
+            alignItems: 'center',
+            flex: 1,
+            alignContent: 'center',
+          }}>
+          <ActivityIndicator size="large" color="blue"></ActivityIndicator>
+        </View>
+      ) : (
+        <View>
+          <View
+            style={{
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginBottom: 10,
+            }}>
+            <Text style={{color: 'black', fontSize: 18, fontWeight: '700'}}>
+              News
+            </Text>
+          </View>
+          <ScrollView>
+            {data.map((mp, j) => {
+              
+              
+              return (
+                <View key={j} style={{}}>
+                  <TouchableOpacity
+                    onPress={() =>
+                      props.navigation.navigate('Homedeatl', {des:mp.description,name:mp.name})
+                    }>
+                    <View>
+                      <Image
+                        source={{
+                          uri: 'https://reactnative.dev/img/tiny_logo.png',
+                        }}
+                        style={{
+                          width: '100%',
+                          height: 150,
+                          resizeMode: 'stretch',
+                        }}></Image>
+                    </View>
+                  </TouchableOpacity>
+
+                  <View
+                    style={{
+                      margin: 10,
+                      justifyContent: 'center',
+                    }}>
+                    <Text style={{fontSize: 15}}>{mp.description}</Text>
+                  </View>
+                </View>
+              );
+            })}
+          </ScrollView>
+        </View>
+      )}
     </View>
   );
-}
+};
+
 export default HomeScreen;
